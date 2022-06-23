a65abf475c323aec4cc2ddddd8caafae48ebdc09
Franpan94 <francescopanico94@libero.it>
2022-06-23 15:50:30 +0200

    problems with the button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 let gridcontainer = document.getElementById('grid-container');
+let button = document.getElementById('button');
 
-
+creategridelementclass(button , 'd-block');
 
 for (let i = 0 ; i < 100 ; i++) {
     let gridbox = creategridbox ();
 
     gridbox.innerHTML = i;
 
+    if(i % 2 === 0) {
+        
+        creategridelementclass(gridbox , 'azure-box');
+    
+    } else {
+
+        creategridelementclass(gridbox , 'red-box');
+    
+    }
+
     gridcontainer.append(gridbox);
 
-    console.log(gridbox);
 }
 
 
@@ -27,4 +37,15 @@ function creategridbox (){
     return grid;
 
 }
+
+function creategridelementclass(gridelement , gridclass) {
+
+     gridelement.addEventListener('click' , function() {
+        
+        gridelement.classList.toggle(gridclass);
+
+     }
+)}
+
+
    
